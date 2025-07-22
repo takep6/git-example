fa160889cbc70ea82dae6a2997c8263cb4385299
takep <msd2019tkhr@gmail.com>
2025-07-23 01:55:48 +0900

    feat: Implement fizzbuzz, add, and sub functions

--- feature.py
+++ feature.py
@@ -9,8 +9,31 @@ def bow():
     print("Bow!")
 
 
+gh issue create - t "Implement fizzbuzz, add, and sub functions" - b "Implement the following functions in feature.py: fizzbuzz, add, sub"
+
+
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
-    # 追加1
-    # 追加2
-    # 追加3
+    fizzbuzz(20)
+    print(add(3, 5))
+    print(sub(10, 4))
     print("let's feature branch!")
