807d0bbced7a1512150528ddc902d1c924dd6d10
takep <74190436+takep6@users.noreply.github.com>
2025-07-29 23:28:38 +0900

    テスト成功

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,12 +1,16 @@
 import pytest
 from rectangle import Rectangle
+from Square import Square
 
 @pytest.fixture
 def rectangle():
     return Rectangle(3, 4)
 
+def square():
+    return Square(3)
+
 def test_area(rectangle):
     assert rectangle.area() == 12
 
-def test_area2(rectangle):
-    assert rectangle.area() == 1
+def test_area_square(square):
+    assert square.area() == 9
